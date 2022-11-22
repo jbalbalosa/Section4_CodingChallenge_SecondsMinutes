10e1d79e7e50dd1f0af94fa511e422f39bce13a7
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-22 14:23:37 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,34 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-        getDurationString(61, 0);
-        getDurationString(150);
+        System.out.println(getDurationString(65, 45));
+        //getDurationString(150);
     }
 
-    public static int getDurationString(int minutes, int seconds){
-        if((minutes < 0) || ((seconds <0) || (seconds > 59))){
+/*    public static int getDurationString(int minutes, int seconds){
+        if((minutes < 0) || (seconds <0) || (seconds > 59)){
             System.out.println("Invalid Value");
             return -1;
         }
+
         int totalHours = minutes / 60;
         int totalMinutes =  ((minutes - (totalHours * 60)) + (seconds / 59));
         System.out.println(totalHours + "h " + totalMinutes + "m " + seconds + "s" );
         return  1;
+    }*/
+
+    //Tim's Solution
+    private  static String getDurationString(long minutes, long seconds) {
+        if ((minutes < 0) || (seconds < 0 || (seconds > 59))) {
+            return "Invalid value";
+        }
+
+        long hours = minutes / 60;
+        long remainingMinutes = minutes % 60;
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 
-    public static int getDurationString(int seconds){
+/*    public static int getDurationString(int seconds){
         if (seconds < 0){
             System.out.println("Invalid value");
             return -1;
@@ -27,5 +39,5 @@ public class Main {
         int totalSecond = seconds- (minutes * 60);
         //System.out.println(minutes + "m " + totalSecond + "s");
         return getDurationString(minutes,totalSecond);
-    }
+    }*/
 }
