0a40f3da897695b0ef3e022e1227752f7170db27
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-23 11:06:25 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
+
 public class Main {
+
+    private  static final String INVALID_VALUE_MESSAGE = "Invalid value";
     public static void main(String[] args) {
         System.out.println(getDurationString(65, 45));
-        //getDurationString(150);
+        //System.out.println(getDurationString(3945L));
+        //System.out.println("Remaining: " + 110 % 60);
     }
 
 /*    public static int getDurationString(int minutes, int seconds){
@@ -11,7 +16,6 @@ public class Main {
             System.out.println("Invalid Value");
             return -1;
         }
-
         int totalHours = minutes / 60;
         int totalMinutes =  ((minutes - (totalHours * 60)) + (seconds / 59));
         System.out.println(totalHours + "h " + totalMinutes + "m " + seconds + "s" );
@@ -21,15 +25,39 @@ public class Main {
     //Tim's Solution
     private  static String getDurationString(long minutes, long seconds) {
         if ((minutes < 0) || (seconds < 0 || (seconds > 59))) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hourString = hours + "h";
+        if (hours < 10){
+            hourString = "0" + hourString;
+        }
+
+        String minuteString = remainingMinutes + "h";
+        if (remainingMinutes < 10){
+            minuteString = "0" + minuteString;
+        }
+
+        String secondString = seconds + "s";
+        if (seconds < 10){
+            secondString = "0" + secondString;
+        }
+        return hourString + " " + minuteString + " " + secondString + "";
     }
 
-/*    public static int getDurationString(int seconds){
+    private static String getDurationString(long seconds){
+        if(seconds < 0){
+            return INVALID_VALUE_MESSAGE;
+        }
+
+        long minutes = seconds / 60;
+
+    }
+
+    /*public static int getDurationString(int seconds){
         if (seconds < 0){
             System.out.println("Invalid value");
             return -1;
